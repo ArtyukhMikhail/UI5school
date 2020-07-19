94dc3cebb51601c5021a50d033ccc0cdcbd5f095
Mikhail Artyukh <darthmix91@mail.ru>
2020-07-19 20:20:03 +0000

    init

---
+++ L05UI5Start/webapp/controller/View1.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("Workspace.L05UI5Start.controller.View1", {
+		
+		onInit: function () {
+		},
+		
+		onClickButton1: function () {
+			var oInput = this.byId("idInput");
+			var oMessageStrip = this.byId("idMessageStrip");
+			
+			 function factorial( n ) {
+				return (n !== 1) ? n * factorial(n - 1) : 1; }
+			//oInput.setValue( factorial(oInput.getValue()) );
+			oMessageStrip.setText("Result: " + factorial(oInput.getValue()) );
+		},
+		
+		onClickButton2: function () {
+			var oInput = this.byId("idInput");
+			var oMessageStrip = this.byId("idMessageStrip");
+			oMessageStrip.setText("Result: ");
+			oInput.setValue("");
+		}
+	});
+});
